60f4583ed7aaa126d87f6c7285d8184593aeeaf1
VMILESC <vmilesc@upv.edu.es>
2025-11-11 00:25:02 +0100

    Añadida implementación de la clase Node

---
+++ Node.h
@@ -0,0 +1,23 @@
+#ifndef NODE_H
+#define NODE_H
+
+#include <ostream>
+
+template <typename T>
+class Node {
+public:
+    T data;
+    Node<T>* next;
+
+    Node(T data, Node<T>* next = nullptr) {
+        this->data = data;
+        this->next = next;
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, const Node<T>& node) {
+        out << node.data;
+        return out;
+    }
+};
+
+#endif
